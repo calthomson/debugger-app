4734698000c61732c963a445b8d0ab26b48d4a8d
calthomson <CaledoniaThomson@gmail.com>
2018-10-01 19:29:20 -0700

    Add docker fake-events-stream container to app, & subscribe to events channel

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,18 @@
-const express = require('express');
+const redis = require('redis');
 
-const app = express();
+const subscriber = redis.createClient();
 
-app.use(express.static('dist'));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+// For now, place new messages on a stack
+const messageStack = [];
+
+// When a new event arrives from Redis via the events channel, add it to the messages stack
+subscriber.on('message', (channel, message) => {
+  messageStack.unshift({ message });
+  console.log('\n\nReceived new event:', message);
+});
+
+// Subscribe to the redis events channel
+subscriber.subscribe('events', (error) => {
+  if (error) throw new Error(error);
+  console.log('Subscribed to the events channel');
+});
